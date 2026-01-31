0b9ee22ecf607d2437d8e796ef13c1de6aa26aa3
Amal124kf <amalfranciskl11@gmail.com>
2026-01-31 13:20:49 +0530

    series

---
+++ hello.py
@@ -0,0 +1,12 @@
+a=float(input("Enter the First Number:"))
+b=float(input("Enter the Second Number:"))
+c=float(input("Enter the Third Number:"))
+
+
+if a>b and a>c:
+	print("a IS THE LARGEST ")
+elif b>a and b>c:
+	print("b IS THE LARGEST ")
+else:
+	print("c IS THE LARGEST ")
+
